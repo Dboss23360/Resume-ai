f4bd118ed723f1ca72d2171072c7f54e079ae2c7
Devon Ross <devonross@mac.mynetworksettings.com>
2025-05-14 15:12:27 -0400

    Update favicon and index.html to use new logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Home from './pages/Home';
 import BuildResume from './pages/BuildResume';
 import Chat from './pages/Chat';
 import RefineResume from './pages/RefineResume';
+import ScrollToTop from './components/ScrollToTop'; // 👈 import the scroll reset
 
 function App() {
     return (
         <Router>
+            <ScrollToTop /> {/* 👈 scroll to top on route change */}
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/build" element={<BuildResume />} />
