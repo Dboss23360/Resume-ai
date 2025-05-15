820e57b31496ce77a3a1fe1df0331d1dc567821d
Devon Ross <devonross@mac.mynetworksettings.com>
2025-05-15 17:25:36 -0400

    Update favicon and index.html to use new logo

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// Your Firebase config
 const firebaseConfig = {
     apiKey: "AIzaSyBCKpZR1GzlF6vL4160hkCWYsZmq7EGnnI",
     authDomain: "myezjobs.firebaseapp.com",
     projectId: "myezjobs",
-    storageBucket: "myezjobs.firebasestorage.app",
+    storageBucket: "myezjobs.appspot.com",
     messagingSenderId: "513283961596",
     appId: "1:513283961596:web:577bfe13aa675145e19d79",
     measurementId: "G-W874RDX1JT"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
